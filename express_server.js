39c3ef5e0460f5b38a3103e0f1feb005e916eaf3
Anaaag <agarc20@outlook.com>
2022-04-21 23:57:35 -0700

    displaying username

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 
 app.set("view engine", "ejs");
+app.use(cookieParser())
 
 function generateRandomString() {
   return Math.random().toString(23).slice(8)
@@ -16,16 +18,19 @@ const urlDatabase = {
 
 
 const bodyParser = require("body-parser");
+const res = require("express/lib/response");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//first route to update 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,  username: req.cookies["username"],};
   res.render("urls_index", templateVars);
 });
 
-
+//third route to update
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 
@@ -41,8 +46,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//second route to update
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],  username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -72,6 +78,19 @@ app.post("/urls/:id", (req, res) => {
   console.log(urlDatabase[req.params.id])
   res.redirect("/urls")
 })
+
+//LOGIN
+app.post("/login", (req, res) => {
+  const username = req.body.username
+  res.cookie("username", username)
+  res.redirect("/urls")
+})
+//LOGOUT
+// app.post("/logout", (req, res) => {
+ 
+//   res.clearCookie(username)
+//   res.redirect("/urls")
+// })
 
 
 app.listen(PORT, () => {
